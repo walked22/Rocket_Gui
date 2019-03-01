f9f60f2f72b8e16ba9dfdbed1e62fe259213c7e1
Walker Davidson <walked22@gmail.com>
2019-02-28 17:33:19 -0800

    Initial Commit

---
+++ testGUI1.py
@@ -0,0 +1,242 @@
+from PyQt5 import QtCore, QtWidgets, QtGui, Qt, uic
+import sys
+import time
+from os import path
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import paho.mqtt.subscribe as subscribe
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+import threading
+from PyQt5.QtCore import QThread, pyqtSignal
+import RPi.GPIO as GPIO
+
+qtCreatorFile = "RocketGUI1.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+HOST = "192.168.0.10"
+port = 1883
+TOPIC_1 = "RELAY"
+TOPIC_2 = "DATA"
+TOPIC_3 = "STATE"
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.IN)
+GPIO.setup(16, GPIO.IN)
+GPIO.setup(18, GPIO.IN)
+GPIO.setup(36, GPIO.IN)
+
+class mainthread(QThread):
+    STATEsignal = pyqtSignal('PyQt_PyObject')
+    DATAsignal = pyqtSignal('PyQt_PyObject')
+
+    def __init__(self):
+        QThread.__init__(self)
+        self.connect1()
+
+    def connect1(self):
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.on_message = self.subscrib1
+        self.mqtt_client.connect(HOST, port=port, keepalive=60)
+        self.mqtt_client.subscribe(TOPIC_2)
+        self.mqtt_client.subscribe(TOPIC_3)
+        self.mqtt_client.loop_start()
+        print("connected")
+
+    def subscrib1(self, mqtt_client, userdata, msg):
+        if msg.topic == "DATA":
+            D = str(msg.payload)[2:-1]
+            C = (D.split(',')[0], D.split(',')[1], D.split(',')[2], D.split(',')[3])
+            C = ('%4s' % C[0], '%4s' % C[1], '%4s' % C[2], '%4s' % C[3])
+            self.DATAsignal.emit(C)
+        if msg.topic == "STATE":
+            B = str(msg.payload)[2:-1]
+            A = (B.split(',')[0], B.split(',')[1], B.split(',')[2], B.split(',')[3], B.split(',')[4], B.split(',')[5], B.split(',')[6])
+            A = ('%4s' % A[0], '%4s' % A[1], '%4s' % A[2], '%4s' % A[3], '%4s' % A[4], '%4s' % A[5], '%4s' % A[6],)
+            self.STATEsignal.emit(A)
+
+class MainApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainApp, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.mythread1 = mainthread()
+        self.init_ui()
+        self.mythread1.STATEsignal.connect(self.progress1)
+        self.mythread1.DATAsignal.connect(self.progress2)        
+        self.radioButton1.toggled.connect(self.radio1)
+        self.radioButton2.toggled.connect(self.radio2)
+        self.radioButton3.toggled.connect(self.radio3)
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.connect(HOST, port=port, keepalive=60)
+
+    def init_ui(self):
+        self.mythread1.start()
+
+    def progress1(self, A):
+        self.Pressure_Key.setAutoFillBackground(True)
+        self.Ign_Safety.setAutoFillBackground(True)
+        self.MPV_Safety.setAutoFillBackground(True)
+        self.MPV_Key.setAutoFillBackground(True)
+        self.Ign_Key.setAutoFillBackground(True)
+        try:
+            if int(A[1]) == 1:
+                p1 = self.Pressure_Key.palette()
+                p1.setColor(self.Pressure_Key.backgroundRole(), Qt.red)
+                self.Pressure_Key.setPalette(p1)
+            if int(A[1]) == 0:
+                p2 = self.Pressure_Key.palette()
+                p2.setColor(self.Pressure_Key.backgroundRole(), Qt.white)
+                self.Pressure_Key.setPalette(p2)
+            if int(A[0]) == 1:
+                p1 = self.Ign_Safety.palette()
+                p1.setColor(self.Ign_Safety.backgroundRole(), Qt.red)
+                self.Ign_Safety.setPalette(p1)
+            if int(A[0]) == 0:
+                p2 = self.Ign_Safety.palette()
+                p2.setColor(self.Ign_Safety.backgroundRole(), Qt.white)
+                self.Ign_Safety.setPalette(p2)
+            if int(A[3]) == 1:
+                p1 = self.MPV_Safety.palette()
+                p1.setColor(self.MPV_Safety.backgroundRole(), Qt.red)
+                self.MPV_Safety.setPalette(p1)
+            if int(A[3]) == 0:
+                p2 = self.MPV_Safety.palette()
+                p2.setColor(self.MPV_Safety.backgroundRole(), Qt.white)
+                self.MPV_Safety.setPalette(p2)
+            if int(A[4]) == 1:
+                p1 = self.MPV_Key.palette()
+                p1.setColor(self.MPV_Key.backgroundRole(), Qt.red)
+                self.MPV_Key.setPalette(p1)
+            if int(A[4]) == 0:
+                p2 = self.MPV_Key.palette()
+                p2.setColor(self.MPV_Key.backgroundRole(), Qt.white)
+                self.MPV_Key.setPalette(p2)
+            if int(A[6]) == 1:
+                p1 = self.Ign_Key.palette()
+                p1.setColor(self.Ign_Key.backgroundRole(), Qt.red)
+                self.Ign_Key.setPalette(p1)
+            if int(A[6]) == 0:
+                p2 = self.Ign_Key.palette()
+                p2.setColor(self.Key.backgroundRole(), Qt.white)
+                self.Ign_Key.setPalette(p2)
+        except:
+            pass
+
+    def progress2(self, C):
+        try:
+            self.Readout0.display(C[0])
+            self.Readout1.display(C[1])
+            self.Readout2.display(C[2])
+            self.Readout3.display(C[3])
+            self.progressBar0.setValue(float(C[0]))
+            self.progressBar1.setValue(float(C[1]))
+            self.progressBar2.setValue(float(C[2]))
+            self.progressBar3.setValue(float(C[3]))
+            if float(C[0]) >= 4500:
+                self.Readout0.setAutoFillBackground(True)
+                p = self.Readout0.palette()
+                p.setColor(self.Readout0.backgroundRole(), Qt.red)
+                self.Readout0.setPalette(p)
+                self.progressBar0.setValue(4500)
+            if float(C[1]) >= 4500:
+                self.Readout1.setAutoFillBackground(True)
+                c = self.Readout1.palette()
+                c.setColor(self.Readout1.backgroundRole(), Qt.red)
+                self.Readout1.setPalette(c)
+                self.progressBar1.setValue(150)
+            if float(C[2]) >= 4500:
+                self.Readout2.setAutoFillBackground(True)
+                h = self.Readout2.palette()
+                h.setColor(self.Readout2.backgroundRole(), Qt.red)
+                self.Readout2.setPalette(h)
+                self.progressBar2.setValue(150)
+            if float(C[3]) >= 4500:
+                self.Readout3.setAutoFillBackground(True)
+                k = self.Readout3.palette()
+                k.setColor(self.Readout3.backgroundRole(), Qt.red)
+                self.Readout3.setPalette(k)
+                self.progressBar3.setValue(150)
+        except:
+            pass
+
+    def radio1(self):
+        self.HE_Range.move(241,284)
+        self.HE_Target.move(263,284)
+        self.HE_T_Label.move(224,325)
+        self.radioButton2.setChecked(False)
+        self.radioButton3.setChecked(False)
+
+    def radio2(self):
+        self.HE_Range.move(747,284)
+        self.HE_Target.move(769,284)
+        self.HE_T_Label.move(713,325)
+        self.radioButton1.setChecked(False)
+        self.radioButton3.setChecked(False)
+
+    def radio3(self):
+        self.HE_Range.move(200,284)
+        self.radioButton1.setChecked(False)
+        self.radioButton2.setChecked(False)
+
+    def lox_mpv_open(self):
+        self.mqtt_client.publish(TOPIC_1,b'LOX_MPV_OPEN')
+        return
+
+    def lox_mpv_close(self):
+        self.mqtt_client.publish(TOPIC_1,b'LOX_MPV_CLOSE')
+        return
+
+    def ch4_mpv_open(self):
+        self.mqtt_client.publish(TOPIC_1,b'CH4_MPV_OPEN')
+        return
+    def ch4_mpv_close(self):
+        self.mqtt_client.publish(TOPIC_1,b'CH4_MPV_CLOSE')
+        return
+
+    def ignitor_on(self):
+        self.mqtt_client.publish(TOPIC_1,b'IGNITOR_ON')
+        return
+
+    def ignitor_off(self):
+        self.mqtt_client.publish(TOPIC_1,b'IGNITOR_OFF')
+        return
+
+    def launch(self):
+        self.mqtt_client.publish(TOPIC_1,b'LAUNCH')
+
+    def abort(self):
+        self.mqtt_client.publish(TOPIC_1,b'ABORT')
+
+    def switch_loop(self):
+        while(1):
+            if (GPIO.input(36) == True):
+              self.lox_mpv_open()
+            else:
+              self.lox_mpv_close()
+            if (GPIO.input(18) == True):
+              self.ch4_mpv_open()
+            else:
+              self.ch4_mpv_close()
+            if (GPIO.input(16) == True):
+              self.ignitor_on()
+            else:
+              self.ignitor_off()
+            if (GPIO.input(12) == True):
+              self.launch()
+            while (GPIO.input(12) == True):
+              pass
+            time.sleep(0.2)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    t1 = threading.Thread(target=MainApp.switch_loop, args=(window, ))
+    t1.start()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
